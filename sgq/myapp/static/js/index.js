52abc3a02b7001f7bc0e390dafae5cd676d417f3
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-05-24 20:00:25 -0300

    .

--- sgq/myapp/static/js/index.js
+++ sgq/myapp/static/js/index.js
@@ -1,22 +1,76 @@
-(() => {
-    const ctx = document.getElementById('myChart');
-      
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
         }]
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
+      legend: {
+        display: false
       }
-    });
+    }
+  })
 })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  
+});
