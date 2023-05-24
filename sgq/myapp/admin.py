aaf3583e5bab81910877ce8298da4c41a35b49dd
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-05-24 20:30:42 -0300

    q

--- sgq/myapp/admin.py
+++ sgq/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Aluno
+from .models import Experimento_Pratico
+from .models import Experimento_Teorico
+
+
+admin.site.register(Aluno)
+admin.site.register(Experimento_Pratico)
+admin.site.register(Experimento_Teorico)
--- sgq/myapp/models.py
+++ sgq/myapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.TextField()
+
+
+    def __str__(self):
+        return self.nome
+
+
+class Experimento_Pratico(models.Model):
+    experimento_p = models.IntegerField(primary_key=True)
+    temp_ebulicao_p = models.FloatField()
+    concentracao_p = models.FloatField(default=None)
+
+    categoria = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+
+
+class Experimento_Teorico(models.Model):
+    experimento_t = models.IntegerField(primary_key=True)
+    temp_ebulicao_t = models.FloatField()
+    concentracao_t = models.FloatField(default=None)
+
+    categoria = models.ForeignKey(Aluno,on_delete=models.CASCADE)
