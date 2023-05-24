0311c5a9c83ce55bbcf2a0d4e40ba0b05c7342eb
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-05-24 20:33:25 -0300

    v

--- sgq/myapp/views.py
+++ sgq/myapp/views.py
@@ -8,3 +8,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+
+
+
+
+
+
+
+def new_exp_pratico():
+    pass
+
+def del_exp_pratico():
+    pass
+
+def new_exp_teorico():
+    pass
+
+def del_exp_teorico():
+    pass
