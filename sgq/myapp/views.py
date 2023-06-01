b80f39a145fb1f0a6e9b06c6ffea449b2204877e
maryane-castro <maryane.castro993@gmail.com>
2023-06-01 16:58:52 -0300

    .

--- sgq/myapp/views.py
+++ sgq/myapp/views.py
@@ -6,17 +6,20 @@ from .models import Aluno, Experimento_Pratico, Experimento_Teorico
 
 def dash(request): 
     x = y = []
+    
     value_teorico = Experimento_Teorico.objects.all()
     for c in value_teorico:
         tempo = 0.53 * c.concentracao_t
-        y.append(round(tempo, 3))
+        y.append(tempo)
         x.append(c.concentracao_t)
+        dict = {'z': x, 'w': y}
+        print(dict)
 
     value_pratico = Experimento_Pratico.objects.all()
     for c in value_pratico:
         pass
 
-    return render(request, 'dash.html', {'y' : y, 'x' : x})
+    return render(request, 'dash.html', {'y' : dict['w'], 'x' : dict['z']})
 
 
 
---
+++ teste/teste.py
@@ -0,0 +1,10 @@
+dic = {"x":10, "y":10 }
+data = []
+
+
+
+
+
+# data.append(dic.copy())
+# dic.clear()
+# print(data, dic)
