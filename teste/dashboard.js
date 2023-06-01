4841ad5b9cd0a45c1f35d16b14571b26b2e7ba56
maryane-castro <maryane.castro993@gmail.com>
2023-06-01 16:31:55 -0300

    .

--- teste/dashboard.js
+++ teste/dashboard.js
@@ -4,6 +4,30 @@
 
 
 
+const data = {
+  datasets: [{
+    label: 'Scatter Dataset',
+    data: [{
+      x: -10,
+      y: 0
+    }, {
+      x: 0,
+      y: 10
+    }, {
+      x: 10,
+      y: 5
+    }, {
+      x: 0.5,
+      y: 5.5
+    }],
+    backgroundColor: 'rgb(255, 99, 132)'
+  }],
+};
+
+
+
+
+
 const ctx = document.getElementById('myChart');
 new Chart(ctx, {
   type: 'scatter',  //line
