bf6ba6726d6a888cc9a35e7efb17e3034d7bb4c8
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-06-04 08:14:54 -0300

    finalizado auth
    parte de autenticação finalizada

--- sgq/myapp/views.py
+++ sgq/myapp/views.py
@@ -1,42 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Aluno, Experimento_Pratico, Experimento_Teorico
 import json
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-def dash(request): 
-    lista_dados = []
-    
-    value_teorico = Experimento_Teorico.objects.all()
-    for c in value_teorico:
-        tempo = 0.53 * c.concentracao_t
-        lista_dados.append({"x": c.concentracao_t, "y":tempo})
+from django.contrib.auth import authenticate 
+from django.contrib.auth import login as lg
 
-    value_pratico = Experimento_Pratico.objects.all()
-    for c in value_pratico:
-        pass
-
-
-
-    dados_json = json.dumps(lista_dados)
-    return render(request, 'dash.html', {"dados_json":dados_json})
-
-
-
-
-
-def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    else:
-        username = request.POST.get('username')
-        senha = request.POST.get('senha')
-
-        user = authenticate(username=username, password=senha)
-        if user:
-            return HttpResponse('autenticado')
-        else: 
-            return HttpResponse('erro')
+#auth
 
 
 
@@ -57,4 +27,40 @@ def cadastro(request):
 
 
 
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
 
+        user = authenticate(username=username, password=senha)
+        if user:
+            lg(request, user)
+            return redirect('dash')
+        else: 
+            return HttpResponse('erro')
+
+
+#plataform
+
+
+def dash(request): 
+    if request.user.is_authenticated:
+        lista_dados = []
+        
+        value_teorico = Experimento_Teorico.objects.all()
+        for c in value_teorico:
+            tempo = 0.53 * c.concentracao_t
+            lista_dados.append({"x": c.concentracao_t, "y":tempo})
+
+        value_pratico = Experimento_Pratico.objects.all()
+        for c in value_pratico:
+            pass
+
+
+
+        dados_json = json.dumps(lista_dados)
+        return render(request, 'dash.html', {"dados_json":dados_json})
+    
+    return HttpResponse('Vc precisa estar logado')
